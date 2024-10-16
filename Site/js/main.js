6ad2f9c5c351256b8bb8570c2737f9ffe1f6a566
RumanLucas2 <lucasruman2@gmail.com>
2024-10-16 17:35:07 -0300

    update

--- Site/js/main.js
+++ Site/js/main.js
@@ -2,8 +2,8 @@
 jQuery(document).ready(function ($) {
 
 //==========================================
-// MOBAILE MENU
-//=========================================
+// MOBILE MENU
+//==========================================
 
     $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -13,10 +13,7 @@ jQuery(document).ready(function ($) {
                 $('html,body').animate({
                     scrollTop: (target.offset().top - 0)
                 }, 1000);
-                if ($('.navbar-toggle').css('display') != 'none') {
-                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                }
-                return false;
+                $(this).parents('.container').find(".navbar-toggle").trigger("click");
             }
         }
     });
@@ -24,7 +21,7 @@ jQuery(document).ready(function ($) {
 
 //==========================================
 //ScrollUp
-//=========================================
+//==========================================
 
     $('#scrollUp').click(function () {
         $("html, body").animate({scrollTop: 0}, 0);
@@ -35,7 +32,7 @@ jQuery(document).ready(function ($) {
 
 //==========================================
 // For fancybox active
-//=========================================
+//==========================================
 
     $('.fancybox').fancybox();
     
@@ -43,11 +40,41 @@ jQuery(document).ready(function ($) {
 
 //==========================================
 // Loading
-//=========================================
+//==========================================
 
     $(window).load(function () {
         $("#loading").fadeOut(1000);
     });
 });
 
+//==========================================
+// Pop Up
+//==========================================
 
+
+// Elementos
+const openBtn = document.getElementById('open-video-btn');
+const popup = document.getElementById('video-popup');
+const closeBtn = document.querySelector('.close-popup');
+const iframe = document.getElementById('video-iframe');
+const HomeVideo = document.getElementById('HomeVideo');
+
+// Abrir o PopUp ao clicar no botão
+openBtn.addEventListener('click', function() {
+    popup.style.display = 'flex';
+    iframe.src = 'https://www.youtube.com/embed/zR9F_TN2b50?autoplay=1'; // URL do vídeo
+});
+
+// Fechar o PopUp ao clicar no botão de fechar
+closeBtn.addEventListener('click', function() {
+    popup.style.display = 'none';
+    iframe.src = ''; // Para parar o vídeo quando o PopUp for fechado
+});
+
+// Fechar o PopUp ao clicar fora do vídeo
+popup.addEventListener('click', function(e) {
+    if (e.target == popup) {
+        popup.style.display = 'none';
+        iframe.src = ''; // Para parar o vídeo
+    }
+});
